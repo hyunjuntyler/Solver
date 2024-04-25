c8bc84cf61a4a6a513eb3475dd9f0057bc1a5bbc
hyunjun <hyunjuntyler@gmail.com>
2024-04-25 22:41:31 +0900

    💄 update content view

--- Solver/Solver/Presentation/View/ContentView.swift
+++ Solver/Solver/Presentation/View/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("onboarding") private var onboarding = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if onboarding {
+            OnboardingView()
+        } else {
+            MainTabView()
         }
-        .padding()
     }
 }
 
