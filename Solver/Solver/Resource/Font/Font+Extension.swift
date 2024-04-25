9751a59603c546c16a06619f5c5f020bb8d9855f
hyunjun <hyunjuntyler@gmail.com>
2024-04-25 22:13:50 +0900

    💄 add font
    add font, font extension

---
+++ Solver/Solver/Resource/Font/Font+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Font+Extension.swift
+//  Solver
+//
+//  Created by hyunjun on 4/25/24.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // MARK: Big icon
+    
+    static let toss3XLarge = Font.custom("TossFaceFontMac", size: 58, relativeTo: .largeTitle)
+    static let toss2XLarge = Font.custom("TossFaceFontMac", size: 52, relativeTo: .largeTitle)
+    static let tossXLarge = Font.custom("TossFaceFontMac", size: 46, relativeTo: .largeTitle)
+    static let tossLarge = Font.custom("TossFaceFontMac", size: 40, relativeTo: .largeTitle)
+    
+    // MARK: Font system
+    
+    static let tossLargeTitle = Font.custom("TossFaceFontMac", size: 34, relativeTo: .largeTitle)
+    static let tossTitle = Font.custom("TossFaceFontMac", size: 28, relativeTo: .title)
+    static let tossTitle2 = Font.custom("TossFaceFontMac", size: 22, relativeTo: .title2)
+    static let tossTitle3 = Font.custom("TossFaceFontMac", size: 20, relativeTo: .title3)
+    static let tossHeadLine = Font.custom("TossFaceFontMac", size: 17, relativeTo: .headline)
+    static let tossBody = Font.custom("TossFaceFontMac", size: 17, relativeTo: .body)
+    static let tossCallout = Font.custom("TossFaceFontMac", size: 16, relativeTo: .callout)
+    static let tossSubHeadline = Font.custom("TossFaceFontMac", size: 15, relativeTo: .subheadline)
+    static let tossFootnote = Font.custom("TossFaceFontMac", size: 13, relativeTo: .footnote)
+    static let tossCaption = Font.custom("TossFaceFontMac", size: 12, relativeTo: .caption)
+    static let tossCaption2 = Font.custom("TossFaceFontMac", size: 11, relativeTo: .caption2)
+}
+
