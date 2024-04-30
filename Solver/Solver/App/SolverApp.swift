52a4e4fe070a48e142f4a228e3ed0078ba149f54
Hyunjun <hyunjuntyler@gmail.com>
2024-04-30 10:20:41 +0900

    🔥 delete init process
    revert process for widget
    Fixing bugs that do not show widgets

--- Solver/Solver/App/SolverApp.swift
+++ Solver/Solver/App/SolverApp.swift
@@ -10,24 +10,12 @@ import SwiftData
 
 @main
 struct SolverApp: App {
-    let container: ModelContainer
-    
-    init() {
-        let schema = Schema([User.self])
-        let config = ModelConfiguration("Solver", schema: schema)
-        do {
-            container = try ModelContainer(for: schema, configurations: config)
-        } catch {
-            fatalError("Could not configure the container")
-        }
-    }
-    
     var body: some Scene {
         WindowGroup {
             RootView {
                 ContentView()
             }
-            .modelContainer(container)
+            .modelContainer(for: [User.self])
         }
     }
 }
