ad68eed236d6ed07ef10b8d7db03715ef6282a56
hyunjun <hyunjuntyler@gmail.com>
2024-07-07 15:43:47 +0900

    ⚡️ update required type from Double to Int

--- Solver/Solver/Presentation/View/MainTab/Component/TierProgress.swift
+++ Solver/Solver/Presentation/View/MainTab/Component/TierProgress.swift
@@ -11,18 +11,18 @@ struct TierProgress: View {
     var tier: Int
     var rating: Int
     
-    private let required: [Double] = [0, 30, 60, 90, 120, 150, 200, 300, 400, 500, 650, 800, 950, 1100, 1250, 1400, 1600, 1750, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2850, 2900, 2950, 3000, 3000.1]
+    private let required: [Int] = [0, 30, 60, 90, 120, 150, 200, 300, 400, 500, 650, 800, 950, 1100, 1250, 1400, 1600, 1750, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2850, 2900, 2950, 3000, 3001]
     
-    private var next: Double {
+    private var next: Int {
         required[tier+1]
     }
     
-    private var current: Double {
+    private var current: Int {
         required[tier]
     }
 
     private var value: Double {
-        (Double(rating) - current) / (next - current)
+        (Double(rating) - Double(current)) / Double(next - current)
     }
     
     var body: some View {
